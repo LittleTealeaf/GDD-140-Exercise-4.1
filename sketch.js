1342fab0d9783c4110098ee656fbecc152c5c0b9
Thomas Kwashnak <shintaomonk@gmail.com>
2022-02-27 21:20:46 -0500

    Added Comments

--- sketch.js
+++ sketch.js
@@ -9,21 +9,29 @@ function setup() {
 
 function draw() {
   background(255);
+
+  //Create the horizontal loop
   for(var i = 0; i < count; i++) {
     if(i%2==0) {
+      //Rectangle of same color
       fill("#FF0000");
       rect(i / count * width,height/2,50,50);
     } else {
+
+      //Ellipse of random color
       fill(random(0,255),random(0,255),random(0,255));
       ellipse(i/count*width,height/2,50,50);
     }
   }
 
+  //Create the vertical loop
   for(var i = 0; i < count; i++) {
     if(i%2==0) {
+      //Rectangle of same color
       fill("#FF0000");
       rect(width/2,i/count*height,50,50);
     } else {
+      //Ellipse of random color
       fill(random(0,255),random(0,255),random(0,255));
       ellipse(width/2,i/count*height,50,50);
     }
@@ -31,6 +39,7 @@ function draw() {
   noLoop();
 }
 
+//Add ability to re-render on loop
 function mousePressed() {
   loop();
 }
